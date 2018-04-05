19aee4af0da6ed86ad0c03a52807ddbce638e4b0
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2018-04-05 17:26:20 -0600

    j

---
+++ Guaflix/Guaflix/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Guaflix.Startup))]
+namespace Guaflix
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
