07760eadeaa7933df78356018fd974f8497eb4c6
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2018-04-18 22:24:31 -0600

    c

--- Guaflix/Guaflix/Controllers/CatalogoController.cs
+++ Guaflix/Guaflix/Controllers/CatalogoController.cs
@@ -113,27 +113,38 @@ namespace Guaflix.Controllers
             searchList.Add(new SelectListItem { Value = "Genero", Text = "Genero" });
             ViewBag.SelectedList = searchList;
            
-            List<Pelicula> peli = new List<Pelicula>();
-            return View(peli);
+            
+            return View(Data.instance.listaBuscados);
         }
 
         // POST: Catologo/Create
         [HttpPost]
         public ActionResult buscar(FormCollection collection)
         {
+            
             try { 
                  if (collection["Eleccion"] == "Nombre")
             {
+                    Pelicula temp = new Pelicula("", collection["filter"], collection["filter2"], "");
+                    Data.instance.listaBuscados.Add(Data.instance.namePelicula.ReturnValor(temp));
+                    Data.instance.listaBuscados.Add(Data.instance.nameShow.ReturnValor(temp));
+                    Data.instance.listaBuscados.Add(Data.instance.nameDocumental.ReturnValor(temp));
 
-            }
+                }
             else if (collection["Eleccion"] == "Año")
             {
-
-            }
+                    Pelicula temp = new Pelicula("", collection["filter2"], collection["filter"], "");
+                    Data.instance.listaBuscados.Add(Data.instance.yearPelicula.ReturnValor(temp));
+                    Data.instance.listaBuscados.Add(Data.instance.yearShow.ReturnValor(temp));
+                    Data.instance.listaBuscados.Add(Data.instance.yearDocumental.ReturnValor(temp));
+                }
             else
             {
-
-            }
+                    Pelicula temp = new Pelicula("", collection["filter2"], "", collection["filter"]);
+                    Data.instance.listaBuscados.Add(Data.instance.genderPelicula.ReturnValor(temp));
+                    Data.instance.listaBuscados.Add(Data.instance.genderShow.ReturnValor(temp));
+                    Data.instance.listaBuscados.Add(Data.instance.genderDocumental.ReturnValor(temp));
+                }
             
                 // TODO: Add insert logic here
 
